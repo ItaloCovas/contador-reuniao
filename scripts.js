3fedbc9370ff5ff3853c681a901da6ea1286d99d
Ítalo Covas <italocovas@gmail.com>
2021-10-05 16:08:20 -0300

    Adicionando arquivos

---
+++ scripts.js
@@ -0,0 +1,32 @@
+const start = document.getElementById('start');
+let minutes = 0;
+let seconds = 0;
+var cron;
+
+
+
+function timer() {
+    
+    seconds++;
+
+    if(seconds == 60) {
+        seconds = 0;
+        minutes++;
+    }
+
+    if(minutes == 1) {
+        clearInterval(cron);
+        alert('O TEMPO ACABOU!');
+    }
+
+    let format = (minutes < 10 ? '0' + minutes : minutes) + ':' + (seconds < 10 ? '0' + seconds : seconds);
+    document.getElementById('texto').innerText = format;
+}
+
+
+function handleClick() {
+    cron = setInterval(timer, 1000);
+}
+
+
+start.addEventListener('click', handleClick);
